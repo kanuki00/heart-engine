5d32ca195f34da647856829962e7d7d7c3a25776
kanuki00 <kalevi.toivanen@gmail.com>
2024-07-23 22:52:21 +0300

    much improved speed in version 2. small animation of mesh moving down

--- he2_types.py
+++ he2_types.py
@@ -15,6 +15,12 @@ class vector3:
         y = self.y-other.y
         z = self.z-other.z
         return vector3(x, y, z)
+    def __mul__(self, other):
+        return vector3(self.x*other, self.y*other, self.z*other)
+    def __truediv__(self, other):
+        return vector3(self.x/other, self.y/other, self.z/other)
+    def __add__(self, other):
+        return vector3(self.x+other.x, self.y+other.y, self.z+other.z)
     def len(self):
         return math.sqrt(dot(self, self))
     def to_string(self):
@@ -35,6 +41,15 @@ class triangle:
         ac = self.c-self.a
         c = cross(ab, ac)
         return abs(c.len()*0.5)
+    def normal(self):
+        ab = self.b - self.a
+        ac = self.c - self.a
+        return normalized(cross(ab, ac))
+        
+class boundbox:
+    def __init__(self, in_a, in_b):
+        self.a = in_a
+        self.b = in_b
         
 def cross(v1, v2):
     x = v1.y*v2.z-v1.z*v2.y
@@ -44,8 +59,11 @@ def cross(v1, v2):
     
 def dot(v1, v2):
     return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
+    
+def normalized(v):
+    return v/v.len()
 
-def point_in_triangle(point, tri):
+def point_in_triangle(point, tri): # could we make this faster?
     tri_a = triangle(point, tri.a, tri.b)
     tri_b = triangle(point, tri.b, tri.c)
     tri_c = triangle(point, tri.c, tri.a)
@@ -56,3 +74,59 @@ def point_in_triangle(point, tri):
         return True
     else:
         return False
+        
+def barycoords(point, tri):
+    u = triangle(point, tri.b, tri.c).get_area() / tri.get_area()
+    v = triangle(point, tri.c, tri.a).get_area() / tri.get_area()
+    w = triangle(point, tri.a, tri.b).get_area() / tri.get_area()
+    return vector3(u, v, w)
+        
+def line_plane_intersect(line_start, line_end, plane_point, plane_normal):
+    epsilon = 0.0001
+    w = line_end - plane_point
+    u = line_end - line_start
+    dot1 = dot(u, plane_normal)
+    dot2 = dot(plane_normal, w)
+    if dot1 > -epsilon and dot1 < epsilon:
+        return None
+    f = dot2/dot1
+    return (u*-f)+line_end
+        
+def safedivide(a, b):
+    if b == 0:
+        return a
+    return a/b
+    
+def project_vec3_vec3(dest, source):
+    d = dot(dest, source)
+    f = d / dest.len()
+    return normalized(dest)*f
+    
+def pp_helper(right, up, proj, plane_loc, ogvert, cam_loc):
+    to_p = proj-plane_loc
+    xdot = dot(right, proj)
+    xsign = safedivide(xdot, abs(xdot))
+    x = project_vec3_vec3(right, to_p).len()/right.len() * xsign
+    ydot = dot(up, proj)
+    ysign = safedivide(ydot, abs(ydot))
+    y = project_vec3_vec3(up, to_p).len()/up.len() * ysign
+    z = -((ogvert-cam_loc).len())
+    return vector3(x, y, z)
+
+def perspective_project(camera, triangles): #TODO camera
+    cam_loc = vector3(2.8, -4.6, 2) # placeholder
+    cam_plane_normal = vector3(-0.48878, 0.797416, -0.353874) # placeholder
+    cam_plane_loc = cam_loc + cam_plane_normal * 4
+    cam_right_vec = normalized(cross(cam_plane_normal, vector3(0,0,1)))
+    cam_up_vec = cross(cam_right_vec, cam_plane_normal)
+    result = []
+    for tri in triangles:
+        result_tri = triangle(vector3(0,0,0), vector3(0,0,0), vector3(0,0,0))
+        proj_a = line_plane_intersect(tri.a, cam_loc, cam_plane_loc, cam_plane_normal)
+        proj_b = line_plane_intersect(tri.b, cam_loc, cam_plane_loc, cam_plane_normal)
+        proj_c = line_plane_intersect(tri.c, cam_loc, cam_plane_loc, cam_plane_normal)
+        result_tri.a = pp_helper(cam_right_vec, cam_up_vec, proj_a, cam_plane_loc, tri.a, cam_loc)
+        result_tri.b = pp_helper(cam_right_vec, cam_up_vec, proj_b, cam_plane_loc, tri.b, cam_loc)
+        result_tri.c = pp_helper(cam_right_vec, cam_up_vec, proj_c, cam_plane_loc, tri.c, cam_loc)
+        result.append(result_tri)
+    return result
--- heart_engine2.py
+++ heart_engine2.py
@@ -4,14 +4,18 @@ import datetime as dt
 from he2_types import vector2
 from he2_types import vector3
 from he2_types import triangle
+from he2_types import boundbox
 from he2_types import cross
 from he2_types import dot
 from he2_types import point_in_triangle
+from he2_types import perspective_project
+from he2_types import normalized
+from he2_types import barycoords
 
-render_resolution = vector2(100, 50)
+render_resolution = vector2(200, 110)
 pixel_list = []
 frame = {}
-exe_time_limit = 10 # seconds
+exe_time_limit = 30 # seconds
 
 # Debug, del later
 frame_example = {"78,47":vector3(255, 255, 0),"0,0":vector3(255, 0, 0), "34,5":vector3(0, 128, 0)}
@@ -25,14 +29,15 @@ def draw(color):
     reset = "\033[0m"
     sys.stdout.write(fg_code+bg_code+"A"+reset)
 
-def draw_frame(in_frame):
+def draw_frame():
     global render_resolution
+    global frame
     sys.stdout.write("\033[H")
     for y in reversed(range(render_resolution.y)):
         for x in range(render_resolution.x):
             try:
                 key = "%d,%d" % (x, y)
-                draw(in_frame[key])
+                draw(frame[key])
             except KeyError:
                 draw(vector3(0,0,0))
         sys.stdout.write("\n")
@@ -43,53 +48,87 @@ def make_pixel_list(resolution):
         for y in range(resolution.y):
             result.append(vector2(x, y))
     return result
+    
+def get_bounds(tris):
+    result = []
+    for tri in tris:
+        ax = min(tri.a.x, tri.b.x, tri.c.x)
+        ay = max(tri.a.y, tri.b.y, tri.c.y)
+        bx = max(tri.a.x, tri.b.x, tri.c.x)
+        by = min(tri.a.y, tri.b.y, tri.c.y)
+        result.append(boundbox(vector2(ax, ay), vector2(bx, by)))
+    return result
+
+def loadmeshjson(file):
+    with open(file) as meshfile:
+        raw_data = json.load(meshfile)
+        result = []
+        for tri in raw_data["Triangles"]:
+            a = tri["A"]
+            b = tri["B"]
+            c = tri["C"]
+            r_tri = triangle(vector3(a[0], a[1], a[2]), vector3(b[0], b[1], b[2]), vector3(c[0], c[1], c[2]))
+            result.append(r_tri)
+        return result
         
-def rasterize_pixel(in_pixel, in_triangles):
+def rasterize_pixel(in_pixel, in_proj_triangles, in_triangles, in_tri_bounds):
     global render_resolution
     global frame
     result = vector3(0,0,0)
     test_x = in_pixel.x/render_resolution.x
     test_y = in_pixel.y/render_resolution.y
     test_loc = vector3(test_x*2-1, test_y*2-1, 0)
-    for tri in in_triangles:
-        tri2D = tri.omit_z()
-        # TODO triangle bounding box optimization
-        if point_in_triangle(test_loc, tri2D):
-            result = vector3(255, 255, 255)
+    shallowest = -10000000000 # TODO
+    #was_drawn = False # TODO
+    for i in range(len(in_proj_triangles)):
+        p_tri = in_proj_triangles[i]
+        p_tri2D = p_tri.omit_z()
+        tri = in_triangles[i]
+        bb = in_tri_bounds[i]
+        if test_loc.x<bb.a.x or test_loc.x>bb.b.x or test_loc.y<bb.b.y or test_loc.y> bb.a.y:
+            # skipping point in tri if test location is not in triangle bounding box
+            continue
+        if point_in_triangle(test_loc, p_tri2D):
+            bary = barycoords(test_loc, p_tri2D)
+            depth = p_tri.a.z*bary.x + p_tri.b.z*bary.y + p_tri.c.z*bary.z
+            if depth > shallowest:
+                shallowest = depth
+                n = tri.normal()
+                shade = max(dot(n, normalized(vector3(0.35, -0.2, 1.0))), 0)
+                col = int(shade*255)
+                result = vector3(col, col, col)
     pixel_keyform = "%d,%d" % (in_pixel.x, in_pixel.y)
     frame[pixel_keyform] = result # setting a dict key that doesn't exist adds the key to the dict
-    
-def pixel_worker(in_start, in_end, tris):
-    global pixel_list
-    end = min(in_end, len(pixel_list))
-    for i in range(in_start, end):
-        pixel = pixel_list[i]
-        rasterize_pixel(pixel, tris)    
-        
-def render_frame():
+
+def render_frame(tris):
     rend_start_time = dt.datetime.now()
-    global render_resolution
-    global frame
-    
-    persp_proj_tris = [
-        triangle(vector3(0, 0.5, 0), vector3(-0.66, -0.33, 0), vector3(0.66, -0.33, 0)),
-        triangle(vector3(0.5, 0.7, 0), vector3(0.1, 0.5, 0), vector3(0.8, -0.33, 0))
-        ]
-    
-    px_max = render_resolution.x*render_resolution.y
-    #pixel_worker(2000, px_max-2000, persp_proj_tris)
-    pixel_worker(0, px_max, persp_proj_tris)
-    
-    draw_frame(frame)
-    sys.stdout.write(str((dt.datetime.now() - rend_start_time)/dt.timedelta(milliseconds=1))+"ms    \n")    
+    global pixel_list
+    # Step 1: perspective projection
+    persp_proj_tris = perspective_project(None, tris)
+    # Step 2: getting projected triangle's bounding boxes
+    ppt_bounds = get_bounds(persp_proj_tris)
+    # Step 3: rasterize pixel and write it into framebuffer
+    for i in range(len(pixel_list)):
+        pixel = pixel_list[i]
+        rasterize_pixel(pixel, persp_proj_tris, tris, ppt_bounds)
+    # Step 4: draw the frame buffer to the screen
+    draw_frame()
+    sys.stdout.write(str((dt.datetime.now() - rend_start_time)/dt.timedelta(milliseconds=1))+"ms    \n")
         
 def main():
     exe_start_time = dt.datetime.now()
     global pixel_list
     global exe_time_limit
     pixel_list = make_pixel_list(render_resolution)
+    clock = 0
     while dt.datetime.now() - exe_start_time < dt.timedelta(seconds=exe_time_limit):
-        render_frame()
+        tris = loadmeshjson("weird_mesh.json")
+        for t in tris:
+            t.a = t.a + vector3(0,0,-0.011)*clock
+            t.b = t.b + vector3(0,0,-0.011)*clock
+            t.c = t.c + vector3(0,0,-0.011)*clock
+        render_frame(tris)
+        clock += 1
     
 if __name__=="__main__":
-    main()
+    exit(main())
