7f9b95d9c6cb0333503eaa2c778fbe947e5795fe
kanuki00 <kalevi.toivanen@gmail.com>
2024-07-23 15:18:28 +0300

    he2 raster success

--- he2_types.py
+++ he2_types.py
@@ -1,8 +1,22 @@
+import math
+
+class vector2:
+    def __init__(self, in_x, in_y):
+        self.x = in_x
+        self.y = in_y
+
 class vector3:
     def __init__(self, in_x, in_y, in_z):
         self.x = in_x
         self.y = in_y
         self.z = in_z
+    def __sub__(self, other):
+        x = self.x-other.x
+        y = self.y-other.y
+        z = self.z-other.z
+        return vector3(x, y, z)
+    def len(self):
+        return math.sqrt(dot(self, self))
     def to_string(self):
         return "x: %f, y: %f, z: %f" % (self.x, self.y, self.z)
         
@@ -11,3 +25,34 @@ class triangle:
         self.a = in_a
         self.b = in_b
         self.c = in_c
+    def omit_z(self):
+        oza = vector3(self.a.x, self.a.y, 0)
+        ozb = vector3(self.b.x, self.b.y, 0)
+        ozc = vector3(self.c.x, self.c.y, 0)
+        return triangle(oza, ozb, ozc)
+    def get_area(self):
+        ab = self.b-self.a
+        ac = self.c-self.a
+        c = cross(ab, ac)
+        return abs(c.len()*0.5)
+        
+def cross(v1, v2):
+    x = v1.y*v2.z-v1.z*v2.y
+    y = v1.z*v2.x-v1.x*v2.z
+    z = v1.x*v2.y-v1.y*v2.x
+    return vector3(x, y, z)
+    
+def dot(v1, v2):
+    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
+
+def point_in_triangle(point, tri):
+    tri_a = triangle(point, tri.a, tri.b)
+    tri_b = triangle(point, tri.b, tri.c)
+    tri_c = triangle(point, tri.c, tri.a)
+    comboarea = tri_a.get_area() + tri_b.get_area() + tri_c.get_area()
+    diff = tri.get_area() - comboarea
+    epsilon = 0.0001
+    if diff > -epsilon and diff < epsilon:
+        return True
+    else:
+        return False
--- heart_engine2.py
+++ heart_engine2.py
@@ -1,10 +1,18 @@
 import sys
+import json
+from he2_types import vector2
 from he2_types import vector3
 from he2_types import triangle
+from he2_types import cross
+from he2_types import dot
+from he2_types import point_in_triangle
 
-render_resolution = vector3(100, 50, 0)
+render_resolution = vector2(100, 50)
 frame = {}
+# Debug, del later
 frame_example = {"78,47":vector3(255, 255, 0),"0,0":vector3(255, 0, 0), "34,5":vector3(0, 128, 0)}
+frame_example["5,27"] = vector3(255, 0, 255)
+# debug end
 
 def draw(color):
     rgb_t = (color.x, color.y, color.z)
@@ -25,4 +33,30 @@ def draw_frame(in_frame):
                 draw(vector3(0,0,0))
         sys.stdout.write("\n")
         
-draw_frame(frame_example)
+def rasterize_pixel(in_pixel, in_frame, in_triangles):
+    global render_resolution
+    result = vector3(0,0,0)
+    test_x = in_pixel.x/render_resolution.x
+    test_y = in_pixel.y/render_resolution.y
+    test_loc = vector3(test_x*2-1, test_y*2-1, 0)
+    for tri in in_triangles:
+        tri2D = tri.omit_z()
+        # TODO triangle bounding box optimization
+        if point_in_triangle(test_loc, tri2D):
+            result = vector3(255, 255, 255)
+    pixel_keyform = "%d,%d" % (in_pixel.x, in_pixel.y)
+    in_frame[pixel_keyform] = result
+        
+def main():
+    global render_resolution
+    global frame
+    persp_proj_tris = [triangle(vector3(0, 0.5, 0), vector3(-0.66, -0.33, 0), vector3(0.66, -0.33, 0))]
+    for x in range(render_resolution.x):
+        for y in range(render_resolution.y):
+            rasterize_pixel(vector2(x, y), frame, persp_proj_tris)
+    draw_frame(frame)
+    print("%fms" % 0.0)
+if __name__=="__main__":
+    main()
+#     print(dot(vector3(0,0,1), vector3(0,1,0)))
+#     print(cross(vector3(0,0,1), vector3(0,1,0)).to_string())
