d07a98a779cca8c8b10d1d4607d0b0ed544cab5c
kanuki00 <kalevi.toivanen@gmail.com>
2024-08-06 17:27:07 +0300

    4x4 matrix inverter works ^U^

--- v4/matrix_inverter.py
+++ v4/matrix_inverter.py
@@ -39,6 +39,17 @@ class vector4:
     def __mul__(self, other):
         return vector4(self.x * other, self.y * other, self.z * other, self.w * other)
 
+    def comp_by_idx(self, i):
+        match i:
+            case 0:
+                return self.x
+            case 1:
+                return self.y
+            case 2:
+                return self.z
+            case 3:
+                return self.w
+
     def print(self):
         sys.stdout.write("[%f %f %f %f]\n" % (self.x, self.y, self.z, self.w))
 
@@ -96,6 +107,23 @@ class vector8:
         self.c = c
         self.d = d
 
+    def __add__(self, other):
+        return vector8(self.x + other.x, self.y + other.y, self.z + other.z, self.w + other.w, self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)
+
+    def __mul__(self, other):
+        return vector8(self.x * other, self.y * other, self.z * other, self.w * other, self.a * other, self.b * other, self.c * other, self.d * other)
+
+    def comp_by_idx(self, i):
+        match i:
+            case 0:
+                return self.x
+            case 1:
+                return self.y
+            case 2:
+                return self.z
+            case 3:
+                return self.w
+
 
 class matrix4x8:
     # [1 0 0 0 | 1 0 0 0]
@@ -114,6 +142,41 @@ class matrix4x8:
         self.r2 = vector8(left.r2.x, left.r2.y, left.r2.z, left.r2.w, right.r2.x, right.r2.y, right.r2.z, right.r2.w)
         self.r3 = vector8(left.r3.x, left.r3.y, left.r3.z, left.r3.w, right.r3.x, right.r3.y, right.r3.z, right.r3.w)
         self.r4 = vector8(left.r4.x, left.r4.y, left.r4.z, left.r4.w, right.r4.x, right.r4.y, right.r4.z, right.r4.w)
+
+    def get_row_by_index(self, index):
+        match index:
+            case 0:
+                return self.r1
+            case 1:
+                return self.r2
+            case 2:
+                return self.r3
+            case 3:
+                return self.r4
+
+    def set_row_by_idx(self, index, new_row_value):
+        match index:
+            case 0:
+                self.r1 = new_row_value
+                return
+            case 1:
+                self.r2 = new_row_value
+                return
+            case 2:
+                self.r3 = new_row_value
+                return
+            case 3:
+                self.r4 = new_row_value
+                return
+
+    def print(self):
+        sys.stdout.write(
+            "[%f %f %f %f | %f %f %f %f]\n" % (self.r1.x, self.r1.y, self.r1.z, self.r1.w, self.r1.a, self.r1.b, self.r1.c, self.r1.d) +
+            "[%f %f %f %f | %f %f %f %f]\n" % (self.r2.x, self.r2.y, self.r2.z, self.r2.w, self.r2.a, self.r2.b, self.r2.c, self.r2.d) +
+            "[%f %f %f %f | %f %f %f %f]\n" % (self.r3.x, self.r3.y, self.r3.z, self.r3.w, self.r3.a, self.r3.b, self.r3.c, self.r3.d) +
+            "[%f %f %f %f | %f %f %f %f]\n" % (self.r4.x, self.r4.y, self.r4.z, self.r4.w, self.r4.a, self.r4.b, self.r4.c, self.r4.d) +
+            "\n"
+        )
 
 
 def im33h1():
@@ -273,29 +336,70 @@ def invert_matrix3x3(in_matrix):
         sys.stdout.write("\n")
 
 
+def get_msr_cands(exclude, in_mat4x8):
+    all_rows = [
+        in_mat4x8.r1,
+        in_mat4x8.r2,
+        in_mat4x8.r3,
+        in_mat4x8.r4
+    ]
+    result = []
+    for i in range(len(all_rows)):
+        if i != exclude:
+            result.append(all_rows[i])
+    return result
+
+
 # TODO
 def invert_mat4x4(in_mat4x4):
+    sys.stdout.write("Origin\n")
     table = matrix4x8(in_mat4x4, matrix4x4())
-    # Reduction order 0 to 15
-    # r1 [12 11  9  8]
-    # r2 [ 0 13 10  7]
-    # r3 [ 1  4 14  6]
-    # r4 [ 2  3  5 15]
-    targets = [
-        table.r2.x, table.r3.x, table.r4.x, table.r4.y, table.r3.y, table.r4.z,
-        table.r3.w, table.r2.w, table.r1.w, table.r1.z, table.r2.z, table.r1.y
+    table.print()
+
+    # Reduction order A to P
+    #     x  y  z  w
+    # r1 [M  L  J  I]
+    # r2 [A  N  K  H]
+    # r3 [B  E  O  G]
+    # r4 [C  D  F  P]
+    tar_row_idxs = [
+        1, 2, 3, 3, 2, 3,
+        2, 1, 0, 0, 1, 0
     ]
     tar_row_comp_idxs = [
         0, 0, 0, 1, 1, 2,
         3, 3, 3, 2, 2, 1
     ]
-    if targets[0] != 0:
-        pass
+    for i in range(12):
+        target = table.get_row_by_index(tar_row_idxs[i]).comp_by_idx(tar_row_comp_idxs[i])
+        if target != 0:
+            msr_cands = get_msr_cands(tar_row_idxs[i], table)  # multiplier source row candidates
+            mult = 1
+            add_cand = vector8(0, 0, 0, 1, 0, 0, 0, 1)
+            for c in msr_cands:
+                cand_comp = c.comp_by_idx(tar_row_comp_idxs[i])
+                if cand_comp != 0:
+                    mult = -target / cand_comp
+                    add_cand = c
+                    break
+            to_add = add_cand*mult
+            row_result = table.get_row_by_index(tar_row_idxs[i]) + to_add
+            table.set_row_by_idx(tar_row_idxs[i], row_result)
+
+            sys.stdout.write("Pass %d\n" % (i+1))
+            table.print()
 
 
-m = matrix3x3()
-m.r1 = vector3(1, 2, -1)
-m.r2 = vector3(2, 5, 1)
-m.r3 = vector3(-1, -2, 2)
+# m = matrix3x3()
+# m.r1 = vector3(1, 2, -1)
+# m.r2 = vector3(2, 5, 1)
+# m.r3 = vector3(-1, -2, 2)
+#
+# invert_matrix3x3(m)
 
-invert_matrix3x3(m)
+m = matrix4x4()
+m.r1 = vector4(1, 2, -1, 0)
+m.r2 = vector4(2, 5, 1, 0)
+m.r3 = vector4(-1, -2, 2, 0)
+
+invert_mat4x4(m)
