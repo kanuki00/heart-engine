27a81d5aed1be334c189cae0cdd69aa9b5b5eca1
kanuki00 <kalevi.toivanen@gmail.com>
2024-08-06 18:32:10 +0300

    fixed inverter bug by always using the row with the shortest vector4 length as the addition candidate. cleanup.

--- v4/matrix_inverter.py
+++ v4/matrix_inverter.py
@@ -1,24 +1,5 @@
+import math
 import sys
-
-
-class vector3:
-    x = 0.0
-    y = 0.0
-    z = 0.0
-
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __add__(self, other):
-        return vector3(self.x + other.x, self.y + other.y, self.z + other.z)
-
-    def __mul__(self, other):
-        return vector3(self.x * other, self.y * other, self.z * other)
-
-    def print(self):
-        sys.stdout.write("[%f %f %f]\n" % (self.x, self.y, self.z))
 
 
 class vector4:
@@ -54,21 +35,6 @@ class vector4:
         sys.stdout.write("[%f %f %f %f]\n" % (self.x, self.y, self.z, self.w))
 
 
-class matrix3x3:
-    #             I    J     K
-    r1 = vector3(1, 0, 0)
-    r2 = vector3(0, 1, 0)
-    r3 = vector3(0, 0, 1)
-
-    def __init__(self):
-        pass
-
-    def print(self):
-        sys.stdout.write("[%f %f %f]\n" % (self.r1.x, self.r1.y, self.r1.z))
-        sys.stdout.write("[%f %f %f]\n" % (self.r2.x, self.r2.y, self.r2.z))
-        sys.stdout.write("[%f %f %f]\n" % (self.r3.x, self.r3.y, self.r3.z))
-
-
 class matrix4x4:
     #             I    J     K     T
     r1 = vector4(1, 0, 0, 0)
@@ -80,10 +46,13 @@ class matrix4x4:
         pass
 
     def print(self):
-        sys.stdout.write("[%f %f %f %f]\n" % (self.r1.x, self.r1.y, self.r1.z, self.r1.w))
-        sys.stdout.write("[%f %f %f %f]\n" % (self.r2.x, self.r2.y, self.r2.z, self.r2.w))
-        sys.stdout.write("[%f %f %f %f]\n" % (self.r3.x, self.r3.y, self.r3.z, self.r3.w))
-        sys.stdout.write("[%f %f %f %f]\n" % (self.r4.x, self.r4.y, self.r4.z, self.r4.w))
+        sys.stdout.write(
+            "[%f %f %f %f]\n" % (self.r1.x, self.r1.y, self.r1.z, self.r1.w) +
+            "[%f %f %f %f]\n" % (self.r2.x, self.r2.y, self.r2.z, self.r2.w) +
+            "[%f %f %f %f]\n" % (self.r3.x, self.r3.y, self.r3.z, self.r3.w) +
+            "[%f %f %f %f]\n" % (self.r4.x, self.r4.y, self.r4.z, self.r4.w) +
+            "\n"
+        )
 
 
 class vector8:
@@ -123,6 +92,12 @@ class vector8:
                 return self.z
             case 3:
                 return self.w
+
+    def get_left_side(self):
+        return vector4(self.x, self.y, self.z, self.w)
+
+    def print(self):
+        sys.stdout.write("[%f %f %f %f | %f %f %f %f]\n" % (self.x, self.y, self.z, self.w, self.a, self.b, self.c, self.d))
 
 
 class matrix4x8:
@@ -174,166 +149,16 @@ class matrix4x8:
             "[%f %f %f %f | %f %f %f %f]\n" % (self.r1.x, self.r1.y, self.r1.z, self.r1.w, self.r1.a, self.r1.b, self.r1.c, self.r1.d) +
             "[%f %f %f %f | %f %f %f %f]\n" % (self.r2.x, self.r2.y, self.r2.z, self.r2.w, self.r2.a, self.r2.b, self.r2.c, self.r2.d) +
             "[%f %f %f %f | %f %f %f %f]\n" % (self.r3.x, self.r3.y, self.r3.z, self.r3.w, self.r3.a, self.r3.b, self.r3.c, self.r3.d) +
-            "[%f %f %f %f | %f %f %f %f]\n" % (self.r4.x, self.r4.y, self.r4.z, self.r4.w, self.r4.a, self.r4.b, self.r4.c, self.r4.d) +
-            "\n"
+            "[%f %f %f %f | %f %f %f %f]\n" % (self.r4.x, self.r4.y, self.r4.z, self.r4.w, self.r4.a, self.r4.b, self.r4.c, self.r4.d)
         )
 
-
-def im33h1():
-    pass
-    # if left_side.r2.x != 0:
-    #     l_candidates = [left_side.r1, left_side.r3]
-    #     r_candidates = [right_side.r1, right_side.r3]
-    #     l_cand = None
-    #     r_cand = None
-    #     for i in range(len(l_candidates)):
-    #         l_c = l_candidates[i]
-    #         r_c = r_candidates[i]
-    #         if l_c.x != 0:
-    #             l_cand = l_c
-    #             r_cand = r_c
-    #             break
-    #     multiplier = -left_side.r2.x / l_cand.x
-    #     to_add_l = l_cand * multiplier
-    #     to_add_r = r_cand * multiplier
-    #
-    #     left_side.r2 = left_side.r2 + to_add_l
-    #     right_side.r2 = right_side.r2 + to_add_r
-    #
-    #     left_side.print()
-    #     sys.stdout.write("\n")
-    #     right_side.print()
-    #     sys.stdout.write("\n")
-
-
-# Gauss Jordan Elimination Method
-def invert_matrix3x3(in_matrix):
-    # Reduction order 0 to 8
-    # [6  5  4]
-    # [0  7  3]
-    # [1  2  8]
-    left_side = in_matrix
-    right_side = matrix3x3()
-    # 0
-    if left_side.r2.x != 0:
-        l_candidates = [left_side.r1, left_side.r3]
-        r_candidates = [right_side.r1, right_side.r3]
-        l_cand = None
-        r_cand = None
-        for i in range(len(l_candidates)):
-            l_c = l_candidates[i]
-            r_c = r_candidates[i]
-            if l_c.x != 0:  # NOTE component
-                l_cand = l_c
-                r_cand = r_c
-                break
-        multiplier = -left_side.r2.x / l_cand.x
-        to_add_l = l_cand * multiplier
-        to_add_r = r_cand * multiplier
-
-        left_side.r2 = left_side.r2 + to_add_l
-        right_side.r2 = right_side.r2 + to_add_r
-
-        left_side.print()
-        sys.stdout.write("\n")
-        right_side.print()
-        sys.stdout.write("\n")
-    # 1
-    if left_side.r3.x != 0:
-        l_candidates = [left_side.r1, left_side.r2]
-        r_candidates = [right_side.r1, right_side.r2]
-        l_cand = None
-        r_cand = None
-        for i in range(len(l_candidates)):
-            l_c = l_candidates[i]
-            r_c = r_candidates[i]
-            if l_c.x != 0:
-                l_cand = l_c
-                r_cand = r_c
-                break
-        multiplier = -left_side.r3.x / l_cand.x
-        to_add_l = l_cand * multiplier
-        to_add_r = r_cand * multiplier
-
-        left_side.r3 = left_side.r3 + to_add_l
-        right_side.r3 = right_side.r3 + to_add_r
-
-        left_side.print()
-        sys.stdout.write("\n")
-        right_side.print()
-        sys.stdout.write("\n")
-    # 2
-    if left_side.r3.y != 0:
-        l_candidates = [left_side.r1, left_side.r2]
-        r_candidates = [right_side.r1, right_side.r2]
-        l_cand = None
-        r_cand = None
-        for i in range(len(l_candidates)):
-            l_c = l_candidates[i]
-            r_c = r_candidates[i]
-            if l_c.y != 0:
-                l_cand = l_c
-                r_cand = r_c
-                break
-        multiplier = -left_side.r3.y / l_cand.y
-        to_add_l = l_cand * multiplier
-        to_add_r = r_cand * multiplier
-
-        left_side.r3 = left_side.r3 + to_add_l
-        right_side.r3 = right_side.r3 + to_add_r
-
-        left_side.print()
-        sys.stdout.write("\n")
-        right_side.print()
-        sys.stdout.write("\n")
-    # 3
-    if left_side.r2.z != 0:
-        l_candidates = [left_side.r1, left_side.r3]
-        r_candidates = [right_side.r1, right_side.r3]
-        l_cand = None
-        r_cand = None
-        for i in range(len(l_candidates)):
-            l_c = l_candidates[i]
-            r_c = r_candidates[i]
-            if l_c.z != 0:
-                l_cand = l_c
-                r_cand = r_c
-                break
-        multiplier = -left_side.r2.z / l_cand.z
-        to_add_l = l_cand * multiplier
-        to_add_r = r_cand * multiplier
-
-        left_side.r2 = left_side.r2 + to_add_l
-        right_side.r2 = right_side.r2 + to_add_r
-
-        left_side.print()
-        sys.stdout.write("\n")
-        right_side.print()
-        sys.stdout.write("\n")
-    # 4
-    if left_side.r1.z != 0:
-        l_candidates = [left_side.r2, left_side.r3]
-        r_candidates = [right_side.r2, right_side.r3]
-        l_cand = None
-        r_cand = None
-        for i in range(len(l_candidates)):
-            l_c = l_candidates[i]
-            r_c = r_candidates[i]
-            if l_c.z != 0:
-                l_cand = l_c
-                r_cand = r_c
-                break
-        multiplier = -left_side.r1.z / l_cand.z
-        to_add_l = l_cand * multiplier
-        to_add_r = r_cand * multiplier
-
-        left_side.r1 = left_side.r1 + to_add_l
-        right_side.r1 = right_side.r1 + to_add_r
-
-        left_side.print()
-        sys.stdout.write("\n")
-        right_side.print()
-        sys.stdout.write("\n")
+    def get_right_side(self):
+        result = matrix4x4()
+        result.r1 = vector4(self.r1.a, self.r1.b, self.r1.c, self.r1.d)
+        result.r2 = vector4(self.r2.a, self.r2.b, self.r2.c, self.r2.d)
+        result.r3 = vector4(self.r3.a, self.r3.b, self.r3.c, self.r3.d)
+        result.r4 = vector4(self.r4.a, self.r4.b, self.r4.c, self.r4.d)
+        return result
 
 
 def get_msr_cands(exclude, in_mat4x8):
@@ -350,18 +175,27 @@ def get_msr_cands(exclude, in_mat4x8):
     return result
 
 
-# TODO
+def dot(v1: vector4, v2: vector4):
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w
+
+
+def length_v4(v: vector4):
+    return math.sqrt(dot(v, v))
+
+
+# Gauss Jordan Elimination Method
 def invert_mat4x4(in_mat4x4):
-    sys.stdout.write("Origin\n")
     table = matrix4x8(in_mat4x4, matrix4x4())
-    table.print()
+    #sys.stdout.write("Origin\n")
+    #table.print()
+    #sys.stdout.write("\n")
 
     # Reduction order A to P
-    #     x  y  z  w
-    # r1 [M  L  J  I]
-    # r2 [A  N  K  H]
-    # r3 [B  E  O  G]
-    # r4 [C  D  F  P]
+    #    x0 y1 z2 w3
+    # r0 [M  L  J  I]
+    # r1 [A  N  K  H]
+    # r2 [B  E  O  G]
+    # r3 [C  D  F  P]
     tar_row_idxs = [
         1, 2, 3, 3, 2, 3,
         2, 1, 0, 0, 1, 0
@@ -376,30 +210,34 @@ def invert_mat4x4(in_mat4x4):
             msr_cands = get_msr_cands(tar_row_idxs[i], table)  # multiplier source row candidates
             mult = 1
             add_cand = vector8(0, 0, 0, 1, 0, 0, 0, 1)
+            shortest_len = 1000000000.0  # TODO
             for c in msr_cands:
                 cand_comp = c.comp_by_idx(tar_row_comp_idxs[i])
                 if cand_comp != 0:
-                    mult = -target / cand_comp
-                    add_cand = c
-                    break
+                    c_len = length_v4(c.get_left_side())
+                    if c_len < shortest_len:
+                        shortest_len = c_len
+                        mult = -target / cand_comp
+                        add_cand = c
             to_add = add_cand*mult
             row_result = table.get_row_by_index(tar_row_idxs[i]) + to_add
             table.set_row_by_idx(tar_row_idxs[i], row_result)
 
-            sys.stdout.write("Pass %d\n" % (i+1))
-            table.print()
+            #sys.stdout.write("Pass %d. Row %d, Column %d\n" % (i+1, tar_row_idxs[i]+1, tar_row_comp_idxs[i]+1))
+            #table.print()
+            #sys.stdout.write("What was added to target's row\n")
+            #to_add.print()
+            #sys.stdout.write("\n")
 
+    return table.get_right_side()
 
-# m = matrix3x3()
-# m.r1 = vector3(1, 2, -1)
-# m.r2 = vector3(2, 5, 1)
-# m.r3 = vector3(-1, -2, 2)
-#
-# invert_matrix3x3(m)
 
 m = matrix4x4()
-m.r1 = vector4(1, 2, -1, 0)
-m.r2 = vector4(2, 5, 1, 0)
-m.r3 = vector4(-1, -2, 2, 0)
+m.r1 = vector4(1, 2, -1, 5)
+m.r2 = vector4(2, 5, 1, 2)
+m.r3 = vector4(-1, -2, 2, -7)
+m.r4 = vector4(0, 0, 0, 1)
+m.print()
 
-invert_mat4x4(m)
+m_i = invert_mat4x4(m)
+m_i.print()
